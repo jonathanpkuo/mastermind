0330e03e1508ff6d853218114d05386267554833
Jonathan Kuo <jonathanpkuo@gmail.com>
2021-10-21 14:00:28 -0700

    game_play_loop sketch & draw_board

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -10,13 +10,39 @@ module Mastermind
     attr_reader :scode
     attr_reader :gboard
     attr_reader :responses
+    attr_reader :display
 
     def initialize
       @scode= Secret.new()
       @gboard = Board.new()
       @responses = Feedback.new()
+      @display = Display.new()
       @is_over = false
       @turn = 1
+    end
+
+    def game_play_loop()
+      # Display board
+      # Take input
+      # Check and formulate responses
+      # Repeat.
+      # Turn > 10 = lose; successfully guess = win.
+
+    end
+
+    def draw_board()
+      gboard.board
+      responses.feedback
+
+      i = 0
+      if gboard.board.length != responses.feedback.length
+        return "Array Mismatch Error"
+      end
+
+      while (i < gboard.board.length) do
+        puts "#{gboard.board[i]} : #{responses.feedback[i]}"
+        i += 1
+      end
     end
   end
 
