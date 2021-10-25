a1b0a92307db62c512c662132741d3045da0a807
Jonathan Kuo <jonathanpkuo@gmail.com>
2021-10-25 11:35:04 -0700

    Simplify wipe_secret function

--- lib/feedback.rb
+++ lib/feedback.rb
@@ -5,6 +5,29 @@ module Mastermind
     def initialize()
       @feedback = Array.new(10, "")
     end
+
+    def c_num(array1, array2)
+      return array1.intersection(array2).length 
+    end
+
+    def c_place(array1, array2)
+      correct = 0
+      array1.each_with_index do | element, index |
+        correct += 1 if element == array2[index]
+      end
+      return correct
+    end
+
+    def bump_data(int, arr_1, arr_2)
+      cor_num = c_num(arr_1, arr_2)
+      cor_pla = c_place(arr_1, arr_2)
+      # feedback[turn] = "There are #{c_num(code_array, input_array)} correct numbers and #{c_place(code_array, input_array)} correct placements."
+      feedback[int] = "There are #{cor_num} correct numbers and #{cor_pla} correct placements."
+    end
+
+    def fuck_you(x, y)
+      puts "#{x} fucks #{y}"
+    end
   end
 
 end
