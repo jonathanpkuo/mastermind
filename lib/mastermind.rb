dd20bc29e74771a8a584cc5f540d739d95e139ec
Jonathan Kuo <jonathanpkuo@gmail.com>
2021-10-26 13:42:17 -0700

    Input validation

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -11,22 +11,26 @@ module Mastermind
     attr_reader :gboard
     attr_reader :responses
     attr_reader :display
+    attr_reader :input
 
     def initialize
       @scode= Secret.new()
       @gboard = Board.new()
       @responses = Feedback.new()
-      @display = Display.new()
+      # @display = Display.new()
+      @input = []
       @is_over = false
-      @turn = 1
+      @turn = 0
+      @buffer = []
     end
 
     def game_play_loop()
+      
       # Display board
       # Take input
       # Check and formulate responses
       # Repeat.
-      # Turn > 10 = lose; successfully guess = win.
+      # Turn > 9 = lose; successfully guess = win.
 
     end
 
@@ -44,6 +48,25 @@ module Mastermind
         i += 1
       end
     end
-  end
 
+    def validator()
+      buffer = []
+      while buffer.length < 4 do
+        input = gets.chomp
+        if input.downcase == "b" && buffer.length > 0
+          buffer.pop()
+        end
+        while ( input.to_i < 0 && input.to_i > 9 )
+          puts "Input Invalid (Move out of scope)."
+          input = gets.chomp
+        end
+        if buffer.length < 4
+          buffer.push(input.to_i)
+        end
+      end
+      return buffer
+    end
+
+
+  end
 end
