047b5d21e8e9a8c7f4aafcdb037c0872c3692209
Jonathan Kuo <jonathanpkuo@gmail.com>
2021-11-15 16:04:29 -0800

    Implement duplicate check to allow user code inputs.

--- lib/input.rb
+++ lib/input.rb
@@ -3,7 +3,14 @@ module Mastermind
     def initialize()
     end
 
-    def input_loop(array)
+    def menu_input()
+
+    end
+
+    def input_loop(array, mode = 0)
+      if mode == 1
+        puts "Secret Code Input"
+      end
       if array.length == 4
         return array
       end
@@ -12,23 +19,29 @@ module Mastermind
       end
       puts "Please input choice."
       input = gets.chomp
+      puts "Your input is: #{input}."
       downcased = proc { |x| x.downcase == "b"}
       scope = proc { |x| x.to_i < 0 || x.to_i > 9 }
       length = proc { |x| x.length < 1 }
+      duplicate = proc { |x| array.any?(x.to_i) && mode == 1}
+      # duplicate = proc { |x| array.any?(x) }
 
       case input
       when downcased
         backspace(array)
-        input_loop(array)
+        input_loop(array, mode)
       when length
         puts "Invalid input."
-        input_loop(array)
+        input_loop(array, mode)
       when scope
         puts "Input out of scope."
-        input_loop(array)
+        input_loop(array, mode)
+      when duplicate
+        puts "Duplicates not permitted."
+        input_loop(array, mode)
       else
         gattai(array, input.to_i)
-        input_loop(array)
+        input_loop(array, mode)
       end
       
     end 
