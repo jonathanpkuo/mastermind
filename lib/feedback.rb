71a1936e13ad1c4b935aebadb2bf6a7e7ce36cb1
Jonathan Kuo <jonathanpkuo@gmail.com>
2022-01-04 14:12:58 -0800

    feedback.rb: Renamed arguments for readability

--- lib/feedback.rb
+++ lib/feedback.rb
@@ -6,28 +6,28 @@ module Mastermind
       @feedback = Array.new(10, "")
     end
 
-    def c_num(array1, array2)
-      return array1.intersection(array2).length 
+    def c_num(code, input)
+      return code.intersection(input).length 
     end
 
-    def c_place(array1, array2)
+    def c_place(code, input)
       correct = 0
-      array1.each_with_index do | element, index |
-        correct += 1 if element == array2[index]
+      code.each_with_index do | element, index |
+        correct += 1 if element == input[index]
       end
       return correct
     end
 
-    def bump_data(int, arr_1, arr_2)
-      cor_num = c_num(arr_1, arr_2)
-      cor_pla = c_place(arr_1, arr_2)
+    def bump_data(turn, code, input)
+      cor_num = c_num(code, input)
+      cor_pla = c_place(code, input)
       if ( cor_pla == 4 )
         # 
-        feedback[int] = "You have guessed the code! You win!"
+        feedback[turn] = "You have guessed the code! You win!"
         return true
       else
         # feedback[turn] = "There are #{c_num(code_array, input_array)} correct numbers and #{c_place(code_array, input_array)} correct placements."
-        feedback[int] = "There are #{cor_num} correct numbers and #{cor_pla} correct placements."
+        feedback[turn] = "There are #{cor_num} correct numbers and #{cor_pla} correct placements."
         return false
       end
     end
